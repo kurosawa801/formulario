ecda96cfbf11b22eac1345d1fa0e6e59d2bb7c62
kurosawa801 <53790344+kurosawa801@users.noreply.github.com>
2020-06-08 12:43:36 -0500

    ejercicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { DatosComponent } from './pages/datos/datos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TemplateComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
